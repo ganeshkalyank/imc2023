a7e466d61471febfef1b11e4032b66c3a22709df
ganeshkalyank <srikgk333@gmail.com>
2023-05-09 15:43:53 +0530

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+var herotext = document.getElementById("herotext");
+
+var typewriter = new Typewriter(herotext, {
+    loop: true,
+    strings: [
+        "Algebra",
+        "Topology",
+        "Functional Analysis",
+        "Differential Equations",
+        "Wavelet Analysis",
+        "Numerical Analysis",
+        "Graph Theory",
+        "Number Theory",
+        "Cryptography",
+        "Stochastic Processes",
+        "Discrete Mathematics",
+        "Data Analysis Techniques",
+        "AI and ML",
+        "Fuzzy Mathematics",
+        "Information Sciences",
+        "Mathematical Modelling",
+        "Scientific Computing",
+        "Soft Computing"
+    ],
+    autoStart: true
+});
